6c70bb1ed0fc6520b2ed2977f5257b0663b4d6ae
Soo Hyun <shkim950921@naver.com>
2017-10-09 23:06:09 +0900

    스위치 액션바

--- app/src/main/java/com/example/kimsoohyun/planiotver01/MenuActivity.java
+++ app/src/main/java/com/example/kimsoohyun/planiotver01/MenuActivity.java
@@ -1,8 +1,10 @@
 package com.example.kimsoohyun.planiotver01;
 
+import android.animation.ObjectAnimator;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -23,6 +26,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.kimsoohyun.planiotver01.Item.MyPlantItem;
+import com.github.lzyzsd.circleprogress.CircleProgress;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,19 +38,23 @@ import java.util.UUID;
 import static com.example.kimsoohyun.planiotver01.R.id.sun_movement_switch;
 
 public class MenuActivity extends AppCompatActivity {
-    private ConnectedThread mConnectedThread;
+   private ConnectedThread mConnectedThread;
 
-
+    CircleProgress circleProgress;
+    CircleProgress circleProgressWater;
     private static final String TAG = "bluetooth";
     final int RECIEVE_MESSAGE = 1;
     private BluetoothAdapter btAdapter = null;
     private BluetoothSocket btSocket = null;
     private StringBuilder sb = new StringBuilder();
     private static int flag = 0;
+    private int sunValue;
+    private int waterValue;
+
     Handler handler;
 
 
-
+    private ImageView fullStatusImage;
     private TextView suntv;
     private TextView watertv;
     byte[] readBuffer;
@@ -71,7 +79,8 @@ public class MenuActivity extends AppCompatActivity {
     private ImageView view;
     private EditText text;
     private Switch switch1;
-
+    private ObjectAnimator waterAnimator;
+    private  ObjectAnimator sunAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,9 +92,12 @@ public class MenuActivity extends AppCompatActivity {
         curtain = (Switch) findViewById(R.id.sun_block_curtain);
         curtainForTemp = (Switch) findViewById(R.id.sun_block_curtain_for_temp);
         watering = (Button) findViewById(R.id.watering);
+        circleProgress = (CircleProgress)findViewById(R.id.sun_circle_progress);
+        circleProgressWater  =(CircleProgress)findViewById(R.id.circle_progress_water);
+        fullStatusImage = (ImageView)findViewById(R.id.fullstatusImage);
 
-        suntv = (TextView)findViewById(R.id.suntxt);
-        watertv = (TextView)findViewById(R.id.watertxt);
+
+
 
         intentFromMyPlantItem = getIntent();
 
@@ -100,6 +112,29 @@ public class MenuActivity extends AppCompatActivity {
                 .load(intentFromMyPlantItem.getStringExtra("img"))
                 .into(view);
 
+      sunAnimator = ObjectAnimator.ofInt(circleProgress,"progress",0,10);
+        sunValue = (Integer)sunAnimator.getAnimatedValue();
+        sunAnimator.setInterpolator(new DecelerateInterpolator());
+        sunAnimator.start();
+
+        waterAnimator = ObjectAnimator.ofInt(circleProgressWater,"progress",0,30);
+        waterValue =(Integer)waterAnimator.getAnimatedValue();
+        waterAnimator.setInterpolator(new DecelerateInterpolator());
+       waterAnimator.start();
+        checkFullStatus();
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MenuActivity.this);
+        dialog.setTitle("자동모드");
+        dialog.setMessage("현재 자동모드입니다. " +
+                " switch가 비활성화 되어있습니다.."
+               );
+        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        dialog.show();
+
 
 
 
@@ -113,11 +148,9 @@ public class MenuActivity extends AppCompatActivity {
                         int endOfLineIndex = sb.indexOf("\r\n");
                         if (endOfLineIndex > 0) {
                             String sbprint = sb.substring(0, endOfLineIndex);
+                            Toast.makeText(MenuActivity.this, sbprint, Toast.LENGTH_SHORT).show();
                             sb.delete(0, sb.length());
-                            Toast toast = Toast.makeText(MenuActivity.this,"블루투스 연결 성공",Toast.LENGTH_SHORT);
-                            toast.show();
-
-
+                            Toast.makeText(MenuActivity.this,"Data From Arduino"+sbprint,Toast.LENGTH_SHORT).show();
                         }
                         break;
                 }
@@ -132,15 +165,19 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(compoundButton.isChecked()){
-                  //  mConnectedThread.write("3");
-                    Toast toast = Toast.makeText(MenuActivity.this,"switch on",Toast.LENGTH_SHORT);
-                    toast.show();
+                    mConnectedThread.write("3");
+                    sunValue = (Integer)sunAnimator.getAnimatedValue();
+                    sunAnimator.setDuration(3000);
+                    sunAnimator.setIntValues(sunValue+30);
+                    sunAnimator.start();
+                    checkFullStatus();
+
+
 
                 }
                 else{
-                 //   mConnectedThread.write("4");
-                    Toast toast = Toast.makeText(MenuActivity.this,"switch off",Toast.LENGTH_SHORT);
-                    toast.show();
+                  mConnectedThread.write("4");
+
                 }
 
             }
@@ -151,15 +188,20 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(compoundButton.isChecked()){
-                   // mConnectedThread.write("1");
-                    Toast toast = Toast.makeText(MenuActivity.this,"switch on",Toast.LENGTH_SHORT);
-                    toast.show();
+                    mConnectedThread.write("1");
+                    TextView temperText = (TextView)findViewById(R.id.temperText);
+                    temperText.setText("28.2도");
+                    temperText.setText("28.4도");
+                    temperText.setText("29.2도");
+                    temperText.setText("26.3도");
+                    temperText.setText("23.2도");
+                    temperText.setText("27.5도");
+
 
                 }
                 else{
-                   // mConnectedThread.write("2");
-                    Toast toast = Toast.makeText(MenuActivity.this,"switch off",Toast.LENGTH_SHORT);
-                    toast.show();
+                    mConnectedThread.write("2");
+
                 }
 
             }
@@ -169,15 +211,18 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(compoundButton.isChecked()){
-                 //   mConnectedThread.write("1");
-                    Toast toast = Toast.makeText(MenuActivity.this,"switch on",Toast.LENGTH_SHORT);
-                    toast.show();
+                   mConnectedThread.write("1");
+
+                    sunValue = (Integer)sunAnimator.getAnimatedValue();
+                    sunAnimator.setDuration(3000);
+                    sunAnimator.setIntValues(sunValue+20);
+                    sunAnimator.start();
+                    checkFullStatus();
 
                 }
                 else{
-                  //  mConnectedThread.write("2");
-                    Toast toast = Toast.makeText(MenuActivity.this,"switch off",Toast.LENGTH_SHORT);
-                    toast.show();
+           mConnectedThread.write("2");
+
                 }
 
             }
@@ -201,17 +246,32 @@ public class MenuActivity extends AppCompatActivity {
         int id = item.getItemId();
         if(id==R.id.mySwitchswitch12){
             if(item.getTitle().toString().equals("수동모드")){
-            Toast.makeText(this,"here",Toast.LENGTH_SHORT).show();
+
             item.setTitle("자동모드");
+          mConnectedThread.write("9");
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MenuActivity.this);
+                dialog.setTitle("자동모드로 전환");
+                dialog.setMessage("자동모드로 전환하였습니다. " +
+                        " 자동모드로 전환하였을경우 switch가 비활성화 됩니다.");
+                dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog.show();
+
+
             lighting.setEnabled(false);
 
             curtainForTemp.setEnabled(false);
             curtain.setEnabled(false);}
             else if(item.getTitle().toString().equals("자동모드")){
-                Toast.makeText(this,"here",Toast.LENGTH_SHORT).show();
+
                 item.setTitle("수동모드");
 
-                AlertDialog.Builder dialog = new AlertDialog.Builder(MenuActivity.this);
+
+
 
 
                 lighting.setEnabled(true);
@@ -230,8 +290,7 @@ public class MenuActivity extends AppCompatActivity {
         super.onPause();
 
        Log.d(TAG, "...In onPause()...");
-        Toast toast = Toast.makeText(MenuActivity.this,"In onPause",Toast.LENGTH_SHORT);
-        toast.show();
+
 
         try {
             btSocket.close();
@@ -263,8 +322,7 @@ public class MenuActivity extends AppCompatActivity {
         super.onResume();
 
         Log.d(TAG, "...onResume - try connect...");
-        Toast toast = Toast.makeText(MenuActivity.this,"onResume",Toast.LENGTH_LONG);
-        toast.show();
+
 
 
        btAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -310,12 +368,12 @@ public class MenuActivity extends AppCompatActivity {
         }
 
         Log.d(TAG, "...Create Socket...");
-        Toast toastconok = Toast.makeText(MenuActivity.this,"Create Socket",Toast.LENGTH_SHORT);
-        toastconok.show();
+        //Toast toastconok = Toast.makeText(MenuActivity.this,"Create Socket",Toast.LENGTH_SHORT);
+        //toastconok.show();
 
         mConnectedThread = new ConnectedThread(btSocket);
         mConnectedThread.start();
-        mConnectedThread.readSensor();
+
 
 
     }
@@ -327,8 +385,7 @@ public class MenuActivity extends AppCompatActivity {
         } else {
             if (btAdapter.isEnabled()) {
                 Log.d(TAG, "...Bluetooth ON...");
-                Toast toastconok = Toast.makeText(MenuActivity.this,"blutooth ON",Toast.LENGTH_SHORT);
-                toastconok.show();
+
             } else {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, 1);
@@ -337,10 +394,32 @@ public class MenuActivity extends AppCompatActivity {
     }
     public void wateringThePlant(View v){
 
-        Toast.makeText(MenuActivity.this, "buttonClicked", Toast.LENGTH_LONG).show();
-      //  mConnectedThread.write("5");
+      //  Toast.makeText(MenuActivity.this, "buttonClicked", Toast.LENGTH_LONG).show();
+        waterValue = (Integer)waterAnimator.getAnimatedValue();
+
+        waterAnimator.setDuration(3000);
+        waterAnimator.setIntValues(waterValue+20);
+        waterAnimator.start();
+
+        checkFullStatus();
+
+
+        mConnectedThread.write("5");
 
     }
+
+    private void checkFullStatus() {
+        if(waterValue>60&&sunValue>60){
+            fullStatusImage.setImageResource(R.drawable.smile);
+        }
+        else if(waterValue>30&&sunValue>30){
+            fullStatusImage.setImageResource(R.drawable.soso);
+        }
+        else{
+            fullStatusImage.setImageResource(R.drawable.angry);
+        }
+    }
+
     public void onSwitchClicked(View view){
         switch(view.getId()){
             case R.id.sun_movement_switch:
@@ -398,7 +477,7 @@ public class MenuActivity extends AppCompatActivity {
         }
 
        public int readSensor(){
-            while(!Thread.currentThread().isInterrupted()){
+
                 try{
                     int byteAvailable = mmInStream.available();
                     if(byteAvailable>0){
@@ -409,7 +488,6 @@ public class MenuActivity extends AppCompatActivity {
                             if(b == mCharDelimiter) {
                             byte[] encodedBytes = new byte[readBufferPosition];
                             System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-
                             final String data = new String(encodedBytes, "US-ASCII");
                             readBufferPosition = 0;
                             handler.post(new Runnable() {
@@ -431,11 +509,11 @@ public class MenuActivity extends AppCompatActivity {
                 catch(IOException e){
 
                 }
-            }
+
 
             int sensorValue=0;
-            toast = Toast.makeText(MenuActivity.this,"data to send",Toast.LENGTH_SHORT);
-            toast.show();
+       //     toast = Toast.makeText(MenuActivity.this,"data to send",Toast.LENGTH_SHORT);
+         //   toast.show();
             try{
                 sensorValue = mmInStream.read();
 
@@ -451,8 +529,8 @@ public class MenuActivity extends AppCompatActivity {
 
         public void write(String message) {
             Log.d(TAG, "...Data to send: " + message + "...");
-            Toast toastconok = Toast.makeText(MenuActivity.this,"data to send",Toast.LENGTH_SHORT);
-            toastconok.show();
+           // Toast toastconok = Toast.makeText(MenuActivity.this,"data to send",Toast.LENGTH_SHORT);
+          //  toastconok.show();
             byte[] msgBuffer = message.getBytes();
             try {
                 mmOutStream.write(msgBuffer);
